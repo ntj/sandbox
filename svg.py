f63cc1d7f7365fe60fc80c10804ddca880fca776
ntj <ntj@allesjetzt.net>
2020-11-20 22:36:57 +0100

    Latest

--- svg.py
+++ svg.py
@@ -257,4 +257,4 @@ if __name__ == "__main__":
         polygon = draw.Drawing(250, 250, origin='center', displayInline=False)
         star1 = draw.Drawing(250, 250, origin='center', displayInline=False)
         star2 = draw.Drawing(250, 250, origin='center', displayInline=False)
-        createPolygon(5, 2, 10, "-" + str(i) + ".svg")
+        createPolygon(8, 2, 10, "-" + str(i) + ".svg")
